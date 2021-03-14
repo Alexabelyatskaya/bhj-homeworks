a6d74646399bccf88d8366b78b4075d702365a58
Alexandra <alexabelyatskaya@yandex.ru>
2021-03-14 19:48:25 +0300

    Исправлено 2, сделано 3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,13 @@
 'use strict'
 
 window.onload = function() {
-	let arr = Array.from(document.getElementsByClassName('menu__item'));
+	let arr = Array.from(document.querySelectorAll('.menu__item'));
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i].querySelector('.menu_sub')) {
-			arr[i].onclick = function() {
-				if (arr[i].querySelector('.menu_active')) {
-					arr[i].querySelector('.menu_sub').className = ('menu menu_sub');
-				} else {
-					arr[i].querySelector('.menu_sub').className = ('menu menu_sub menu_active');
-					}
+			arr[i].querySelector('.menu__link').onclick = function(event) {
+				arr[i].querySelector('.menu_sub').classList.toggle('menu_active');
 				return false;
-			};	
+			};
 		}
 	}
 };
---
+++ element-search/slider/task.js
@@ -0,0 +1,14 @@
+'use strict'
+
+window.onload = function() {
+	let counter = 0;
+	let arr = Array.from(document.querySelectorAll('.slider__item'));
+	document.querySelector('.slider__arrow_next').onclick = function() {
+		counter = arr.findIndex((currentValue) => {
+			return currentValue.classList.contains('slider__item_active');
+		});
+		arr[counter].classList.toggle('slider__item_active');
+		counter = (counter + 1) % arr.length;
+		arr[counter].classList.toggle('slider__item_active');
+	}
+}
