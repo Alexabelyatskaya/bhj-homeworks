ab4770d3c464c26cd309644fcc1f061da64fa352
Alexandra <alexabelyatskaya@yandex.ru>
2021-05-17 20:25:41 +0300

    Правки 2

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -12,11 +12,14 @@ window.onload = function(){
 		product.querySelector('.product__quantity-control_dec').onclick = function(){
 			let counter = product.querySelector('.product__quantity-value');
 			let counterValue = parseInt(counter.innerText);
-			counter.innerText = counterValue - 1 > 0 ?   counterValue-1 : 0;
+			counter.innerText = counterValue - 1 > 0 ? counterValue - 1 : 0;
 		};
 
-		product.querySelector('.product__add').onclick = function(){
-			
+		product.querySelector('.product__add').onclick = function(){			
+			if (parseInt(product.querySelector('.product__quantity-value').innerText) == 0) {
+				return false;
+			}
+
             let newItem = document.createElement('div');
             newItem.classList.add('cart__product');
             newItem.dataset['id'] = product.dataset['id'];
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,10 +3,13 @@
 window.onload = function() {
 	let arr = Array.from(document.querySelectorAll('.has-tooltip'));
 	for (let element of arr) {
-		element.onclick = function() {
+		element.onclick = function(event) {
 			let hint = document.querySelector('.tooltip');
-			hint.innerText = event.currentTarget.getAttribute('title');
 			hint.classList.toggle('tooltip_active');
+			if(hint.innerText !== event.currentTarget.getAttribute('title')){
+				hint.innerText = event.currentTarget.getAttribute('title');
+				hint.classList.add('tooltip_active');
+			}
 			hint.style.left = event.currentTarget.getBoundingClientRect().left + 'px';
 			hint.style.top = event.currentTarget.getBoundingClientRect().bottom + 'px';
 			return false;
